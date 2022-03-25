de5dbb5ef83ed37acbe61d8e088447864be14018
Lance Wolfe <lwolfe@talentpath.com>
2022-03-25 15:22:35 -0400

    Set up controller

--- src/main/java/com/jsonsbikes/shop/controllers/BikesController.java
+++ src/main/java/com/jsonsbikes/shop/controllers/BikesController.java
@@ -1,13 +1,63 @@
 package com.jsonsbikes.shop.controllers;
 
+import com.jsonsbikes.shop.models.Bike;
 import com.jsonsbikes.shop.repositories.BikeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/bikes")
 public class BikesController {
     @Autowired
     private BikeRepository bikeRepository;
+
+    @GetMapping("/")
+    List<Bike> list() {
+        return bikeRepository.findAll();
+    }
+
+    @GetMapping("/{vin}")
+    Bike get(@RequestParam String vin) {
+        return bikeRepository.findByVin(vin);
+    }
+
+    @GetMapping("/{make}")
+    List<Bike> listMake(@RequestParam String make) {
+        return bikeRepository.findByMake(make);
+    }
+
+    @GetMapping("/{type}")
+    List<Bike> listType(@RequestParam String type) {
+        return bikeRepository.findByType(type);
+    }
+
+    @PostMapping("/")
+    Bike newBike(@RequestBody Bike newBike) {
+        return bikeRepository.saveAndFlush(newBike);
+    }
+
+    @PutMapping("/{bike_id}")
+    Bike update(@RequestBody Bike updatedBike, @PathVariable Long id) {
+        return bikeRepository.findById(id)
+                .map(bike -> {
+                    bike.setMake(updatedBike.getMake());
+                    bike.setPrice(updatedBike.getPrice());
+                    bike.setType(updatedBike.getType());
+                    bike.setPurchased(updatedBike.getPurchased());
+                    bike.setVin(updatedBike.getVin());
+                    return bikeRepository.saveAndFlush(bike);
+                }).orElseGet(() -> {
+                    updatedBike.setBike_id(id);
+                    return bikeRepository.saveAndFlush(updatedBike);
+                });
+    }
+
+    @DeleteMapping("/{bike_id}")
+    void delete(@PathVariable Long id) {
+        bikeRepository.deleteById(id);
+    }
+
+
 }
--- src/main/java/com/jsonsbikes/shop/repositories/BikeRepository.java
+++ src/main/java/com/jsonsbikes/shop/repositories/BikeRepository.java
@@ -1,4 +1,15 @@
 package com.jsonsbikes.shop.repositories;
 
-public interface BikeRepository {
+import com.jsonsbikes.shop.models.Bike;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BikeRepository extends JpaRepository<Bike, Long> {
+
+    Bike findByVin(String vin);
+
+    List<Bike> findByMake(String make);
+
+    List<Bike> findByType(String type);
 }
